dfa70652b60d32eeecf347ce89f0771717b24ef2
Thalita coelho <thalita.bastos94@gmail.com>
2024-06-11 14:30:52 -0300

    pontos iniciais

---
+++ src/conexao.js
@@ -0,0 +1,12 @@
+// conexão com o banco de dados
+const { Pool } = require('pg')
+
+const pool = new Pool({
+	host: 'localhost',
+	port: 5432,
+	user: 'postgres',
+	password: '174029',
+	database: 'catalogo_pokemons'
+})
+
+module.exports = pool
